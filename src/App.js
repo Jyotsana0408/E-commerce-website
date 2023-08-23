f70d37e3cfa60068555ed7a403ef98f589a0b7b8
Jyotsna0408 <jyotsnasahu16@gmail.com>
2023-08-23 11:38:07 +0530

    add cart component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import AvailableProduct from "./components/Content/AvailableProduct";
+import Header from "./components/Header/Header.js";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header/>
+      <AvailableProduct/>
+    </React.Fragment>
+);
+}
+export default App;
